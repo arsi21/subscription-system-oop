b72a1a905190be142448f90e1c9f4e08ef8985b6
Rouen Quirino <rouen2101@gmail.com>
2022-05-24 15:46:55 +0800

    add comment in login.php

--- models/login.php
+++ models/login.php
@@ -4,12 +4,14 @@ class Login extends Dbh {
     protected function getUser($username, $password){
         $stmt = $this->connect()->prepare('SELECT * FROM user WHERE username = ? AND password = ?;');
 
+        //check if success
         if(!$stmt->execute(array($username, $password))){
             $stmt = null;
             header("location: ../index.php?error=stmtFailed");
             exit();
         }
 
+        //check if password and username match
         if($stmt->rowCount() == 0){
             $stmt = null;
             header("location: ../index.php?error=didNotMatch");
@@ -20,6 +22,7 @@ class Login extends Dbh {
 
         session_start();
 
+        //set session
         $_SESSION['id'] = $user[0]['id'];
         $_SESSION['username'] = $user[0]['username'];
         $_SESSION['access'] = $user[0]['access'];
