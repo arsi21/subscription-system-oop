ee8cab15b835b5e7c464b01f564e7725814c9938
Rouen Quirino <rouen2101@gmail.com>
2022-05-24 19:17:00 +0800

    add transactions model

---
+++ models/transactions.php
@@ -0,0 +1,34 @@
+<?php
+
+class Transactions extends Dbh {
+    protected function getTransaction(){
+        $stmt = $this->connect()->query('SELECT subscription.subscription_name, transaction.amount, transaction.paid_date, transaction.id, transaction.status
+        FROM subscription
+        INNER JOIN invoice
+        ON invoice.subscription_id = subscription.id
+        AND invoice.status = "paid"
+        INNER JOIN transaction
+        ON transaction.invoice_id = invoice.id
+        INNER JOIN user
+        ON transaction.user_id = user.id;');
+        $result = $stmt->fetchAll();
+
+        return $result;
+    }
+
+    protected function getTransactionTotal(){
+        $stmt = $this->connect()->query('SELECT subscription.subscription_name, transaction.amount, transaction.paid_date, transaction.id, transaction.status
+        FROM subscription
+        INNER JOIN invoice
+        ON invoice.subscription_id = subscription.id
+        AND invoice.status = "paid"
+        INNER JOIN transaction
+        ON transaction.invoice_id = invoice.id
+        INNER JOIN user
+        ON transaction.user_id = user.id;');
+        $transactionCount = $stmt->rowCount();
+
+        return $transactionCount;
+    }
+
+}
