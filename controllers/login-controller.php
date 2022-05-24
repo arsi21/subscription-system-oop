8d4661faa56bc5215eaad9d24635c02e5e0b364b
Rouen Quirino <rouen2101@gmail.com>
2022-05-24 15:37:54 +0800

    add login controller

---
+++ controllers/login-controller.php
@@ -0,0 +1,33 @@
+<?php
+
+class LoginController extends Login {
+    private $username;
+    private $password;
+    
+    public function __construct($username, $password){
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    public function loginUser() {
+        if($this->emptyInput() == false){
+            //redirect to
+            header("location: ../index.php?error=emptyInput");
+            exit();
+        }
+
+        $this->getUser($this->username, $this->password);
+    }
+
+    private function emptyInput() {
+        $result;
+
+        if(empty($this->username) || empty($this->password)){
+            $result = false;
+        }else{
+            $result = true;
+        }
+
+        return $result;
+    }
+}
