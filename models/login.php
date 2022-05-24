6b1366001a6f144cf44e5725524f60d3cab85949
Rouen Quirino <rouen2101@gmail.com>
2022-05-24 15:37:16 +0800

    add login model

---
+++ models/login.php
@@ -0,0 +1,29 @@
+<?php
+
+class Login extends Dbh {
+    protected function getUser($username, $password){
+        $stmt = $this->connect()->prepare('SELECT * FROM user WHERE username = ? AND password = ?;');
+
+        if(!$stmt->execute(array($username, $password))){
+            $stmt = null;
+            header("location: ../index.php?error=stmtFailed");
+            exit();
+        }
+
+        if($stmt->rowCount() == 0){
+            $stmt = null;
+            header("location: ../index.php?error=didNotMatch");
+            exit();
+        }
+
+        $user = $stmt->fetchAll();
+
+        session_start();
+
+        $_SESSION['id'] = $user[0]['id'];
+        $_SESSION['username'] = $user[0]['username'];
+        $_SESSION['access'] = $user[0]['access'];
+
+        $stmt = null;
+    }
+}
