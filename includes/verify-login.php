523d9c08a05d63b8f6e6d31644848126548ded86
Rouen Quirino <rouen2101@gmail.com>
2022-05-24 15:38:47 +0800

    add verify-login

---
+++ includes/verify-login.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST['loginBtn'])){
+    //get data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //Instantiate Class
+    include "../lib/dbh.php";
+    include "../models/login.php";
+    include "../controllers/login-controller.php";
+    $login = new LoginController($username, $password);
+
+    //Error handler and login
+    $login->loginUser();
+
+    //Redirect to page
+    header("location: ../index.php?error=none");
+}
