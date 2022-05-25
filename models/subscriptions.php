c4953543525452b92b904250cfe30a3af9768429
Rouen Quirino <rouen2101@gmail.com>
2022-05-25 08:25:46 +0800

    add subscriptions model

---
+++ models/subscriptions.php
@@ -0,0 +1,22 @@
+<?php
+
+class Subscriptions extends Dbh {
+    public function getSubscriptions($id){
+        $stmt = $this->connect()->prepare('SELECT * FROM subscription
+        WHERE user_id = ?;');
+        $stmt->execute(array($id));
+        $results = $stmt->fetchAll();
+
+        return $results;
+    }
+
+    public function getSubscriptionsCount(){
+        $stmt = $this->connect()->prepare('SELECT * FROM subscription
+        WHERE user_id = ?;');
+        $stmt->execute(array($id));
+        $result = $stmt->rowCount();
+
+        return $result;
+    }
+
+}
